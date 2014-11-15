639c8a894afaeb79a5fa58a20db67d2c8374dba7
Danila Poyarkov <dannotemail@gmail.com>
2014-11-15 12:13:59 +0300

    initial commit

---
+++ template.lua
@@ -0,0 +1,26 @@
+local template = {}
+
+function template.print(data, args, callback)
+  local callback = callback or print
+  local function exec(data)
+    if type(data) == "function" then
+      setfenv(data, args or {})
+      data(exec)
+    else
+      callback(data)
+    end
+  end
+  exec(data)
+end
+
+function template.parse(data)
+  return "return function(_) _[=[" ..
+    data:gsub("#%(", "]=]_("):gsub("%)#", ")_[=["):gsub("#%[", "]=] "):gsub("%]#", " _[=[") ..
+    "]=] end"
+end
+
+function template.compile(data)
+  return loadstring(template.parse(data))()
+end
+
+return template
